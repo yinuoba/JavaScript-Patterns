870eac9547520661d061170e2a2c156b4190d075
Hoogle <zeng.xianghu@hotmail.com>
2012-11-27 10:56:10 +0800

    update
    添加曾经tom大叔在博客上发布的几个经典题目

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ some demos about JavaScript patterns
 * newobject.js 实例化构造函数的全过程
 * initbranches.js 初始化分支是一种优化模式，当知道某个条件在整个生命周期内都不会发生变化时，仅对该条件测试一次。
 * cachefunction.js 备忘模式 让复杂的操作缓存在函数的属性中，从而只需要执行一遍。
+* typicalapp.js tom大叔曾经在博客上给出的几个经典题目
---
+++ typicalapp.js
@@ -0,0 +1,74 @@
+/**
+ * 1、找出数字数组中最大的元素（使用Math.max函数）
+2、转化一个数字数组为function数组（每个function都弹出相应的数字）
+3、给object数组进行排序（排序条件是每个元素对象的属性个数）
+4、利用JavaScript打印出Fibonacci数（不使用全局变量）
+5、实现如下语法的功能：var a = (5).plus(3).minus(6); //2
+6、实现如下语法的功能：var a = add(2)(3)(4); //9
+ */
+
+
+//找出数字数组中最大的元素（使用Math.max函数）
+var arr = [3, 6, 1, 54, 2, 4],
+	max = Math.max.apply(null, arr);
+console.log(max);
+
+
+//转化一个数字数组为function数组（每个function都弹出相应的数字）
+var arr = [3, 6, 1, 54, 2, 4],
+	fun = [];
+for(var a in arr) {
+	fun.push(function() {
+		alert(a)
+	});
+}
+console.log(fun);
+
+
+// 给object数组进行排序（排序条件是每个元素对象的属性个数）
+var arr1 = ["a", "w"],
+	arr2 = ["a", "d", "w", "s"],
+	arr3 = ["a"],
+	arr4 = ["a", "d", "w", "e", "f", "sa"];
+var arr = [arr1, arr2, arr3, arr4];
+
+function sortArr(a, b) {
+	return b.length - a.length
+}
+arr.sort(sortArr);
+
+
+// 利用JavaScript打印出Fibonacci数（不使用全局变量）
+function fibonacci(n) {
+	return(function(n) {
+		if(n == 1 || n == 2) return 1;
+		return arguments.callee(n - 1) + arguments.callee(n - 2);
+	})(n);
+}
+fibonacci(10); //55
+
+
+// 实现如下语法的功能：var a = (5).plus(3).minus(6); //2
+Number.prototype.plus = function(xld) {
+	return this + xld
+};
+Number.prototype.minus = function(xld) {
+	return this - xld
+};
+var a = (5).plus(3).minus(6);
+console.log(a);
+
+
+// 实现如下语法的功能：var a = add(2)(3)(4);
+function add(x) {
+	add.valueOf = add.toString = function() {
+		return x;
+	}
+
+	function add(y) {
+		x += y;
+		return add;
+	}
+	return add;
+}
+add(2)(5)(8);
