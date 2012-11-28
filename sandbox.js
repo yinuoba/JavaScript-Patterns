44d0a20458efcce30fc1f7c124722a634f948853
Hoogle <zeng.xianghu@hotmail.com>
2012-11-28 10:02:42 +0800

    update

--- sandbox.js
+++ sandbox.js
@@ -3,7 +3,7 @@
 	 * 沙箱模式
 	 */
 	// 定义Sandbox构造函数
-	function Sandbox(){
+	function Sandbox() {
 		var args = Array.prototype.slice.call(arguments),
 			// 最后一个是回调函数
 			callback = args.pop(),
@@ -21,7 +21,7 @@
 
 		// 给this添加模块
 		// 如果指定模块名称或模块名称为*，则运用于所有模块
-		if(typeof modules === 'undefined' || modules[0] === '*'){
+		if(typeof modules === 'undefined' || modules[0] === '*') {
 			modules = [];
 			for(i in Sandbox.modules) {
 				if(Sandbox.modules.hasOwnProperty(i)) {
@@ -32,11 +32,11 @@
 
 		// 初始化需要的模块
 		for(var i = 0, length = modules.length; i < length; i++) {
-			Sandbox.modules[modules[i]](this);
+			Sandbox.modules[modules[i]](this)
 		}
 
 		// 执行callback
-		callback(this);
+		callback(this)
 	}
 
 	// 添加原型扩展
@@ -58,7 +58,7 @@
 	Sandbox.modules.dom = function(box) {
 		box.getElement = function() {};
 		box.getStyle = function() {};
-		box.foo = "bar";
+		box.foo = "bar"
 	};
 
 	// 添加事件模块
@@ -66,13 +66,13 @@
 		// 如果需要，就访问sandbox的原型
 		// box.constructor.prototype.m = 'mmm';
 		box.attachEvent = function() {};
-		box.detachEent = function() {};
+		box.detachEent = function() {}
 	};
 
 	// 添加ajax模块
 	Sandbox.modules.ajax = function(box) {
 		box.makeRequest = function() {};
-		box.getResponse = function() {};
+		box.getResponse = function() {}
 	};
 
 
@@ -89,17 +89,17 @@
 
 	// 运用沙箱 3
 	Sandbox('dom', 'event', 'ajax', function(box) {
-		console.info(box);
+		console.info(box)
 	});
 
 	// 运用沙箱 4
 	Sandbox('*', function(box) {
-		console.info(box);
+		console.info(box)
 	});
 
 	// 运用沙箱 5
 	Sandbox(function(box) {
-		console.info(box);
+		console.info(box)
 	});
 
 	// 运用沙箱 6
@@ -107,6 +107,6 @@
 		// 使用dom和事件来运行
 		Sandbox('ajax', function(box) {
 			// 这里的box与外部的box不一样
-		});
-	});
+		})
+	})
 })();
