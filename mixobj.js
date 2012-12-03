2174d9bb6ad2d05cb0756ffb215992f314060da9
Hoogle <zeng.xianghu@hotmail.com>
2012-12-03 17:22:48 +0800

    update

--- mixobj.js
+++ mixobj.js
@@ -3,14 +3,14 @@
 	 * 将多个对象合并为一个对象，将多个对象的属性连接起来
 	 * @return {Object} 返回混合后的对象
 	 */
+
 	function mix() {
 		var arg = arguments,
 			length = arg.length - 1,
-			prop, 
-			mixobj = {};
-		for(; length>=0; length--) {
+			prop, mixobj = {};
+		for(; length >= 0; length--) {
 			var nowObj = arg[length];
-			for(prop in nowObj){
+			for(prop in nowObj) {
 				if(nowObj.hasOwnProperty(prop)) {
 					mixobj[prop] = nowObj[prop];
 				}
@@ -21,9 +21,9 @@
 	var obj = mix({
 		name: "Hoogle",
 		age: 24
-	},{
-		getName: function(){}
-	},{
+	}, {
+		getName: function() {}
+	}, {
 		arr: [1, 3, 5, [1, 3, 5]],
 	});
 
